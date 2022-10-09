f34ef2351a0823bc6302c692a28054642c9703ab
nabilraihann <namura152@gmail.com>
2022-10-09 10:59:11 +0700

    add intent HomeActivity.java ke ProfileActivity.java dan sebaliknya

--- app/src/main/java/com/uasppm/sanggraloka/ProfileActivity.java
+++ app/src/main/java/com/uasppm/sanggraloka/ProfileActivity.java
@@ -1,8 +1,13 @@
 package com.uasppm.sanggraloka;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        bottomNavigationView.setSelectedItemId(R.id.profil_activity_page);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.home_activity_page:
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.profil_activity_page:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
