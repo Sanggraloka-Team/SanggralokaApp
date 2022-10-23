4157cc7dfec745f6466c2ad2584008be7db9178b
komangshinta <komangshinta2002@gmail.com>
2022-10-23 09:29:52 +0700

    add isi data destinasi wisata floating market

--- app/src/main/java/com/uasppm/sanggraloka/DestinasiWisataData.java
+++ app/src/main/java/com/uasppm/sanggraloka/DestinasiWisataData.java
@@ -2,28 +2,31 @@ package com.uasppm.sanggraloka;
 
 public class DestinasiWisataData {
     private static String[] namaDestinasiWisata = {
-            "",
+            "Floating Market",
             "",
             "",
             "",
     };
 
     private static String[] alamatDestinasiWisata = {
-            "",
+            "Jl. Grand Hotel No.33E, Lembang, Kec. Lembang, Kabupaten Bandung Barat, Jawa Barat 40391",
             "",
     };
 
     private static String[] deskripsiDestinasiWisata = {
-            "",
+            "Tingkatkan pengalaman berbelanja Anda di Floating Market Lembang di mana Anda dapat berbelanja di atas air. Rasakan sensasi unik berbelanja barang-barang seperti makanan lokal di atas perahu yang mengapung di sepanjang danau yang indah menggunakan koin Pasar Terapung yang dibuat khusus untuk pembelian Anda. Selain berbelanja, Floating Market juga menawarkan aktivitas seru lainnya yang bisa dicoba segala usia.\n" +
+                    "\n" +
+                    "Mulai dari batagor, rujak, hingga ronde jahe dan kebab, Anda bisa menemukan berbagai suguhan lezat untuk memuaskan selera Anda. Jelajahi atraksi lain di sekitar pasar, seperti wahana air di danau dan Taman Pelangi dengan spot foto outdoor yang indah. Lebih baik lagi, sewa kostum Kyotoku, lalu berjalan-jalan dengan kimono ini bersama orang yang Anda cintai dan rasakan seolah-olah Anda berada di taman Jepang.",
             "",
     };
     private static String[] fasilitasDestinasiWisata = {
-            "",
+            "Toilet, Musholla, ATM, Restoran, Tempat Ganti Pakaian, Area Parkir",
             "",
     };
 
     private static int[] imageDestinasiWisata = {
             R.drawable.img_floating_market,
             R.drawable.img_de_ranch,
+            R.drawable.img_orchid_forest_cikole
     };
 }
