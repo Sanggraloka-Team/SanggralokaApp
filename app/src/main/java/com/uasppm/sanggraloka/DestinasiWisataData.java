8ea31548868846d2e5d44d3393e083cf651a4106
komangshinta <komangshinta2002@gmail.com>
2022-10-23 10:04:39 +0700

    isi data destinasi wisata (kurang foto Sanghyang Heuleut)

--- app/src/main/java/com/uasppm/sanggraloka/DestinasiWisataData.java
+++ app/src/main/java/com/uasppm/sanggraloka/DestinasiWisataData.java
@@ -3,30 +3,128 @@ package com.uasppm.sanggraloka;
 public class DestinasiWisataData {
     private static String[] namaDestinasiWisata = {
             "Floating Market",
-            "",
-            "",
-            "",
+            "De Ranch",
+            "Orchid Forest Cikole",
+            "Dapoer Pandan Wangi",
+            "Burgreens",
+            "Sapulidi",
+            "Jalan Dago",
+            "Paris Van Java",
+            "The Secret",
+            "Tebing Keraton",
+            "Sanghyang Heuleut",
+            "Situ Patenggang",
+            "Curug Maribaya",
+            "The Great Asia Africa",
+            "Branchsto Ganesha"
     };
 
     private static String[] alamatDestinasiWisata = {
             "Jl. Grand Hotel No.33E, Lembang, Kec. Lembang, Kabupaten Bandung Barat, Jawa Barat 40391",
-            "",
+            "Jl. Maribaya No.17, Kayuambon, Lembang, Kabupaten Bandung Barat, Jawa Barat 40391",
+            "Genteng, Cikole, Kec. Lembang, Kabupaten Bandung Barat, Jawa Barat 40391",
+            "Jl. Patuha No.38, Lkr. Sel., Kec. Lengkong, Kota Bandung, Jawa Barat 40263",
+            "Jl. Cimanuk No.6, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40115",
+            "Kompleks Graha Puspa, Jl. Sersan Bajuri, Cihideung, Kec, Parongpong, Kabupaten Bandung Barat, Jawa Barat 40559",
+            "Jalan Ir H Djuanda, Kecamatan Coblong, Kota Bandung",
+            "Paris Van Java Jl. Sukajadi, 40161, Jawa Barat",
+            "The Secret, Jln. LL. RE. Martadinata (Riau) No. 47, Kota Bandung, Jawa Barat, Indonesia",
+            "Puncak Kordon, RT.2/RW.10, Ciburial, Kec. Cimenyan, Bandung Barat, Jawa Barat 40198",
+            "PLTA Saguling, Rajamandala Kulon, cipatat, Kabupaten Bandung Barat",
+            "Jalan Raya Ciwidey – Rancabali, Kecamatan Rancabali, Kabupaten Bandung, Jawa Barat 40973",
+            "Kampung Cikondang Lamajang Pangalengan, Cibodas, Kec. Lembang, Kabupaten Bandung Barat, Jawa Barat 40391",
+            "The Great Asia Africa, Jl. Raya Lembang - Bandung No.71, Gudangkahuripan, Lembang, Kabupaten Bandung Barat, Jawa Barat 40391, Indonesia",
+            "Jl. Padasuka Atas No.KM 4, RW.006, Cimenyan, Kec. Cimenyan, Kota Bandung, Jawa Barat 40391"
+
     };
 
     private static String[] deskripsiDestinasiWisata = {
             "Tingkatkan pengalaman berbelanja Anda di Floating Market Lembang di mana Anda dapat berbelanja di atas air. Rasakan sensasi unik berbelanja barang-barang seperti makanan lokal di atas perahu yang mengapung di sepanjang danau yang indah menggunakan koin Pasar Terapung yang dibuat khusus untuk pembelian Anda. Selain berbelanja, Floating Market juga menawarkan aktivitas seru lainnya yang bisa dicoba segala usia.\n" +
                     "\n" +
                     "Mulai dari batagor, rujak, hingga ronde jahe dan kebab, Anda bisa menemukan berbagai suguhan lezat untuk memuaskan selera Anda. Jelajahi atraksi lain di sekitar pasar, seperti wahana air di danau dan Taman Pelangi dengan spot foto outdoor yang indah. Lebih baik lagi, sewa kostum Kyotoku, lalu berjalan-jalan dengan kimono ini bersama orang yang Anda cintai dan rasakan seolah-olah Anda berada di taman Jepang.",
-            "",
+            "Konsep tempat wisata keluarga di Bandung ini mengusung konsep Western/Cowboy. Kita bisa bermain atau menunggang kuda, berfoto dengan kostum koboy yang tersedia, mengelilingi area dengan kuda, dan lain sebagainya.\n" +
+                    "\n" +
+                    "Pastinya, anak-anak bakal senang banget bermain disini. Dengan konsep yang lumayan beda dengan tempat lainnya di Lembang, tentu bisa memberikan suasana segar bagi anda yang berlibur kesini.",
+
+            "Konsep tempat wisata keluarga di Bandung ini mengusung konsep Western/Cowboy. Kita bisa bermain atau menunggang kuda, berfoto dengan kostum koboy yang tersedia, mengelilingi area dengan kuda, dan lain sebagainya.\n" +
+                    "\n" +
+                    "Pastinya, anak-anak bakal senang banget bermain disini. Dengan konsep yang lumayan beda dengan tempat lainnya di Lembang, tentu bisa memberikan suasana segar bagi anda yang berlibur kesini.",
+
+            "Orchid Forest Cikole Lembang sudah berdiri sejak Agustus tahun 2017. Tempat ini merupakan taman anggrek terluas di Indonesia. Berada di tengah kawasan hutan lindung dan terbentang seluas 12 hektar. Setidaknya terdapat 157 jenis bunga anggrek beraneka macam dikembangkan di sini.\n" +
+                    "\n" +
+                    "Orchid Forest Cikole Bandung memfokuskan diri untuk memperkenalkan dan membudidayakan berbagai tanaman anggrek. Menggunakan metode lokal maupun internasional. Spesies Anggreknya tidak hanya berasal dari Indonesia yang memang merupakan negara kedua terbanyak memiliki varian anggrek. Tanaman anggrek di Orchid Forest juga berasal dari negara lain, seperti Venezuela, Argentina, Filipina, Peru, dan Amerika serikat.",
+
+            "Dapoer Pandan Wangi berbeda dengan rumah makan lainnya melainkan rumah makan ini terkenal dengan rumah makan sunda dengan dekorasi ala pedesaan dengan suasana yang nyaman serta udara yang segar . Dapoer Pandan Wangi ini sering di rekomendasikan oleh banyak orang sehingga jangan kaget kalau rumah makan sunda ini penuh akan pengunjung, tetapi anda tidak perlu khawatir Dapoer Pandan Wangi memiliki tempat yang besar dan luas. \n" +
+                    "\n" +
+                    "Menu yang tersedia cukup banyak dan bervariasi seperti olahan ayam, sapi, seafood, tumisan serta berbagai macam minuman. Tidak hanya itu, untuk anda yang ingin menu praktis di Dapoer Pandan Wangi menyediakan menu paket. Menu yang wajib anda coba menurut yatu Terong raos, Sambel dadak, Ikan bakar sambal kecap dan Tumis tauge.",
+
+            "Burgreens merupakan salah satu restoran makanan vegan yang dirancang khusus untuk lidah orang Asia, untuk anda yang sudah memulai menjaga makanan resto ini perlu anda kunjungi.  \n" +
+                    "\n" +
+                    "Walaupun menunya terfragmentasi, menariknya pengunjung terbesar bukan vegetarian, melainkan orang-orang yang sadar terhadap kesehatan. Jadi untuk anda yang bukan vegetarian atau sedang berusaha untuk clean eating burgreens jadi pilihan tempat yang tepat. Banyak berbagai menu di Burgreens anda dapat memilih dengan sesuka hati, menu yang wajib dicoba yaitu Barley quinoa with vegan yakitori dengan saladnya yang segar serta tekstur vegan yakitori yang pas dan lezat menu ini sangan direkomendasika.",
+
+            "Sapu Lidi yang terletak di Lembang menyajika tempat makan di bandung yang mencitrakan objek wisata dengan konsep Traditional Ethnic budaya khas Jawa Barat. Anda dapat merasakan suasana khas jawa barat, sehingga Sapu lidi ini bisa dikatakan sebagai salah  satu rumah makan sunda di bandung. Resto ini dibuat unik anda dapat merasakan seolah-olah anda makan di tengah sawah, lengkap dengan danau dan sampan-sampan yang boleh dipakai pengunjung.\n" +
+                    "\n" +
+                    "Konsep rumah makan sunda ini dirancang dan didekorasi dengan sifat pedesaan yang nyaman, dicampur dengan gairah kenikmatan kuliner dan makanan yang menarik. Anda dapat menikmati berbagai makanan khas, seperti jamur jadi ngeunah (jamur tiram digoreng tepung dan disiram saus gurih), lalu ikan bakar daun (ikan air tawar dibumbui, kemudian dibungkus daun pisang dan dibakar, seperti pepes, tetapi tidak dikukus dulu), dan sapi gigibrik (mirip empal daging sapi supergurih).",
+
+            "Ir. H. Djuanda merupakan salah satu jalan raya utama yang berada di Kota Bandung. Jalan utama dalam sebuah kota biasanya dilengkapi dengan pedestrian untuk menjadi tempat lalu-lalang para pejalan kaki. Pedestrian merupakan salah satu aspek penting dalam kemajuan sebuah kota. Oleh sebab itu, setiap kota biasanya memperlengkapi pedestrian di setiap jalan raya utama, dimana hal tersebut sangat mempengaruhi citra sebuah kota.",
+
+            "Paris Van Java, salah satu mal besar di Sukajadi, Bandung, esmi dibuka pada Juli 2006. Dikembangkan oleh PT Bintang Bangun Mandiri, mal ini menerapkan arsitektur mediterania yang terdiri dari empat lantai dan uniknya dinamai menjadi Concourse Level, Glamour Level, Resort Level, dan Sky Level. Pusat perbelanjaan dengan total luas pertokoan 4.700 meter persegi ini diisi oleh lebih dari 200 tenants, yang menawarkan barang dan jasa.",
+
+            "Di bawah manajemen yang sama dengan The Summit, factory outlet The Secret memiliki desain bangunan yang lebih misterius sehingga menarik perhatian para pengunjung. Nuansa hitam yang ditonjolkan serta koridor dengan suasana gelap saat masuk membuat pengunjung semakin penasaran. Akan tetapi, setelah melewati koridor tersebut, pengunjung akan disambut dengan nuansa ceria yang dipantulkan dari warna-warni aksesoris dan variasi model pakaian.\n" +
+                    "\n" +
+                    "Tak hanya itu, di bagian dalamnya, pengunjung akan dimanjakan dengan pemandangan segar dari taman terbukanya. Di taman terbuka ini terdapat beberapa tempat duduk untuk menunggu, kolam ikan yang cukup besar, dan beberapa kedai makanan untuk mengisi perut yang kosong. Selain produk-produk The Secret yang berkualitas, tempatnya yang unik dan nyaman pun menjadi salah satu alasan factory outlet ini banyak digemari para wisatawan domestik maupun mancanegara.",
+
+            "Tebing Keraton atau Tebing Karaton merupakan sebuah tebing yang berada di dalam kawasan Taman Hutan Raya Ir. H. Djuanda. Tebing ini terletak di Kampung Ciharegem Puncak, Desa Ciburial, Bandung, Jawa Barat, Indonesia. Dari Tebing Keraton dapat menikmati pemandangan spektakuler. Bukan lampu kota, melainkan hutan!\n" +
+                    "\n" +
+                    "Tidak sulit untuk mencapai Tebing Keraton. Dari pusat kota Bandung, bertolaklah ke arah Dago Pakar, kemudian ke arah Taman Hutan Raya Ir. H. Djuanda. Setelah pintu gerbang Taman Hutan Raya Ir. H. Djuanda, beloklah ke kanan. Anda akan melewati rumah-rumah besar dan kemudian perkampungan. Setelah itu Anda akan sampai di sebuah warung dengan baliho besar bertuliskan 'Warung Bandrek' alias Warban. Perjalanan belum selesai, pacu kendaraan Anda melewati tanjakan dan jalan berbatu sampai pos teratas, langsung di Tebing Keraton.",
+
+            "Sanghyang heuleut merupakan sebuah danau kecil dengan air berwarna biru kehijauan yang dikelilingi oleh bebatuan besar. Konon, danau ini merupakan tempat mandinya bidadari yang turun dari kahyangan.Nama sanghyang heuleut diartikan sebagai tempat suci yang dikunjungi bidadari yang memiliki waktu berbeda dengan manusia. Ini dapat dilihat dari kata “sanghyang” yang mempunyai arti “sesuatu yang dianggap suci” dan “heuleut” artinya “selang antara dua waktu”. Sanghyang heuleut konon merupakan danau purba yang merupakan hasil dari letusan gunung api purba yang dikenal gunung Sunda. Air danau sanghyang heuleut berasal dari aliran sungai Citarum lama, danau kecil yang berwarna biru dengan pemandangan alam yang cantik ini kadangkala berubah menjadi hijau toska.",
+
+            "Situ Patengan adalah suatu danau yang terletak di kawasan objek wisata alam Bandung Selatan, danau ini cocok untuk anda yang ingin melihat pemandangan alam yang sangat eksotik serts hamparan Kebun Teh Bandung khas Rancabali. Situ patenggang selama ini selalu menjadi destinasi wisata favorit di Ciwidey bagi pengunjung asal perkotaan sebagai tempat rekreasi bersama keluarga sekaligus tempat refreshing untuk melepas penat. \n" +
+                    "\n" +
+                    "Jika anda sedang beruntung, cuaca di kawasa wisata danau dekat perkebunan teh rancabali ini akan diselimuti kabut putih yang membuat tempat ini sangat begitu cantik. Menikmati pesona keindahan situ patenggang juga bisa anda nikamti dengan cara duduk di gazebo beralaskan tikar sembari botram menikmati bekal makanan yang sudah kamu persiapkan sebelumnya. Keseruan berada di danau indah di rancabali ciwidey ini akan semakin sempurna jika dilakukan bersama – sama dengan keluarga, teman atau berdua dengan pasangan.",
+
+            "Curug atau Air Terjun Maribaya memiliki ketinggian kurang lebih 15 meter. Ketinggian Curug ini menambah daya tarik tersendiri untuk para pengunjung. Untuk anda yang ingin melihat hempasan air terjun yang tinggi dan membuat badan anda basar terkena air curug ini cocok anda kunjungi. Di curug ini, kamu juga bisa melihat banyak bebatuan dengan berbagai ukuran. Bebatuan yang besar dan kecil menambah keindahan tempat ini. Namun, kamu juga perlu berhati-hati saat berjalan atau duduk di atas batu. Pasalnya, batu yang terkena cipratan air terjun cenderung licin dan berlumut.\n" +
+                    "\n" +
+                    "Tidak hanya dapat melihat ketingggiannya saja, anda juga dapat menikmati kesegaran dari Air Terjun Maribaya dan bisa menikmati kesegaran airnya dengan cara berenang di kolam penampungan air Curug. Kolam penampungan air dari Curug indah ini cukup dalam, jika tidak bisa berenang disarankan tidak berenang di kolam ini. Kamu bisa berenang di area kolam curug, pada saat musim kemarau. Pasalnya, jika berenang pada musim hujan, air kolam dari Curug ini sangat keruh akibat banyaknya lumpur.",
+
+            "Mencari pelarian cepat tanpa harus meninggalkan negara ini? Datang dan kunjungi The Great Asia Afrika dimana kamu bisa menjelajahi 10 negara dalam 1 tempat. Rasakan pengalaman berjalan di sekitar desa tradisional Jepang, kota merah muda India, dan banyak tempat luar biasa lainnya yang tidak akan pernah Anda lupakan. Terletak di Lembang, Bandung, objek wisata ini menawarkan pemandangan indah dari setiap sudutnya. Ajak orang-orang terkasih dan jangan lupa mengabadikan setiap momen yang Anda habiskan di sini melalui lensa kamera Anda!",
+
+            "Menggabungkan peternakan dan restoran di satu tempat, Branchsto Ganesha Equestrian menawarkan pengunjung pengalaman unik di mana mereka dapat mencoba berbagai kegiatan berkuda dan menikmati santapan bergaya pedesaan. Di sini, Anda bisa naik kereta anjing bersama keluarga dan menunggangi kuda atau kuda poni untuk si kecil di bawah bimbingan instruktur terlatih. Lebih dari itu, Branchsto Ganesha Equestrian juga menawarkan kesempatan untuk mencoba kelas berkuda untuk meningkatkan keterampilan berkuda Anda!"
     };
     private static String[] fasilitasDestinasiWisata = {
             "Toilet, Musholla, ATM, Restoran, Tempat Ganti Pakaian, Area Parkir",
-            "",
+            "Area Parkir, Toilet, Tempat Istirahat",
+            "Restoran, Area Parkir, Toilet",
+            "Toilet, Area Merokok, Ruangan VIP, Area Parkir",
+            "Toilet, Area Parkir, Wifi",
+            "Toilet, Wifi, Area Merokok, Area Outdor, Area Parkir",
+            "Toilet, Wifi, Area Merokok, Area Outdor, Area Parkir",
+            "Tempat Parkir Basement, Cafes, Tempat Antar/Jemput, Lift lobby, Pintu Masuk Utama, Keamanan 24 Jam",
+            "Toilet, Wifi, Area Merokok, Area Outdor, Area Parkir",
+            "Mushola, Toilet umum, Area parkir, Restoran",
+            "area parkir, toilet, musholla",
+            "Toilet, Gazebo, Toko Souvenir, Mushola, Tempat Parkir",
+            "Area Parkir, Toilet, Mushola, Resto",
+            "Musholla, Toilet, Area Parkir, Restoran, ATM",
+            "ATM, Musholla, Area Parkir, Toilet, Wifi, Restoran"
     };
 
     private static int[] imageDestinasiWisata = {
             R.drawable.img_floating_market,
             R.drawable.img_de_ranch,
-            R.drawable.img_orchid_forest_cikole
+            R.drawable.img_orchid_forest_cikole,
+            R.drawable.img_dapoer_pandan_wangi,
+            R.drawable.img_burgreens,
+            R.drawable.img_sapulidi,
+            R.drawable.img_jalan_dago,
+            R.drawable.img_paris_van_java_mall,
+            R.drawable.img_the_secret,
+            R.drawable.img_tebing_keraton,
+            R.drawable.img_floating_market,
+            R.drawable.img_situ_patenggang,
+            R.drawable.img_curug_maribaya,
+            R.drawable.img_the_great_asia_africa,
+            R.drawable.img_branchsto_ganesha
     };
 }
